f8a5d1543f52080846dceeb4447e2924d18052f5
mac <mac@macdeMacBook-Pro.local>
2018-05-09 06:45:31 +0800

    bundle

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var path = require("path");
+
+module.exports = {
+  target: 'node',
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "index.js"
+  }
+};
