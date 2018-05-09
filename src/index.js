a6949560e856e5f88c802a3ab753583c07f9b889
mac <mac@macdeMacBook-Pro.local>
2018-05-09 22:30:25 +0800

    add fetch runtime

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+var fetch = require('weapp-fetch');
 var regeneratorRuntime = require('weapp-regenerator-rumtime');
 
 module.exports = {
+  fetch: fetch,
   regeneratorRuntime: regeneratorRuntime,
 }
